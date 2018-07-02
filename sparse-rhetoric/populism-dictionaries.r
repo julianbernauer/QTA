193ad69a74f14189ca23a309a1bc88bfa1991f0e
Julian Bernauer <julian.bernauer@googlemail.com>
2018-07-02 16:27:35 +0200

    Create populism-dictionaries.r

---
+++ sparse-rhetoric/populism-dictionaries.r
@@ -0,0 +1,253 @@
+# populism dictionaries in german, english, frensh, spanish and italian 
+
+pop_dict_deu <- dictionary(list(
+people = c("*volk*"),
+commun = c("*bürger*",
+"*einbürger*",
+"*steuerzahler*",
+"*gemeinschaft*"),
+elites = c("*elit*",
+"*politiker*",
+"*establishment*",
+"*herrschend*"),
+corrupt = c("*korrupt*",
+"*propagand*",
+"*täusch*",
+"*betrügen*",
+"*verrat*",
+"*scham*",
+"*schämen*",
+"*skandal*",
+"*wahrheit*",
+"*unehrlich*",
+"*lüge*"),        
+integrit = c("*verantw*",
+"*glaubwürdig*"),
+independ = c("*souverän*",
+"*neutral*"),
+dirdem = c("*demokratisch*",
+"*referendum*",
+"*volksabstimmung*",
+"*volksinitiative*"),
+delib = c("*konsens*",
+"*kompromiss*"),
+represent = c("*representation*",
+"*parlament*",
+"*regierung*"),
+antipop = c("*populist*",
+"*demagog*")                                              
+))                         
+
+
+  pop_dict_eng <- dictionary(list(
+    
+    people = c("*people*"),
+    
+    commun = c("*citizen*",
+               "*nationalise*",
+               "*taxpayer*",
+               "*community*"),
+    
+    elites = c("*elite*",
+               "*politician*", 
+               "*establishment*",
+               "*ruling*"),
+    
+    corrupt = c("*corrupt*",
+                "*propaganda*",
+                "*deceit*",
+                "*deceive*",
+                "*betray*",
+                "*shame*",
+                "*scandal*",
+                "*truth*",
+                "*dishonest*",
+                "*lying*",
+                "*fake*"),        
+    
+    integrit = c("*responsibility*",
+                 "*credibility*"
+                 ),
+    
+    independ = c("*sovereignty*",
+                 "*neutral*"),
+    
+    dirdem = c("*democratic*",
+               "*referendum*",
+               "*popular*",
+               "*vote*",
+               "*plebiscite*"),
+    
+    delib = c("*consens*",
+              "*compromise*"),
+    
+    represent = c("*representation*",
+                  "*parliament*",
+                  "*government*"),
+    
+    antipop = c("*populist*",
+                "*demagog*")                                              
+    
+  ))        
+  
+    
+  
+    pop_dict_fra <- dictionary(list(
+    
+    people = c("*peuble*"),
+    
+    commun = c("*citoyen*",
+               "*naturalis*",
+               "*contribuabl*",
+               "*communaut*", 
+               "*collectivit*"),
+    
+    elites = c( "*elit*",
+                "*politic*",
+                "*establishment*",
+                "*regnant*"),
+    
+    corrupt = c("*corromp*",
+                "*propagand*",
+                "*trompe*", 
+                "*abuse*", 
+                "*fraude*",
+                "*rahir*",
+                "*honte*",
+                "*scandal*",
+                "*verit*",
+                "*malhonnet*", 
+                "*mensong*",
+                "*faux*"),        
+    
+    integrit = c("*responsabil*",
+                 "*credibil*"),
+    
+    independ = c("*souverainet*",
+                 "*neutr*"),
+    
+    dirdem = c("*democratiqu*",
+               "*referend*",
+               "*plebiscit*"),
+    
+    delib = c("*consens*",
+              "*compromis*"),
+    
+    represent = c("*represent*",
+                  "*parlement*",
+                  "*gouvernement*"),
+    
+    antipop = c("*populist*",
+                "*demagog*")                                              
+    
+  ))        
+  
+    
+      
+  
+  
+    pop_dict_esp <- dictionary(list(
+    
+    people = c("*puebl*"),
+    
+    commun = c("*ciudad*",
+               "*implantac*",
+               "*tributant*",
+               "*communid*"),
+    
+    elites = c("*elit*",
+               "*politic*",
+               "*establiciement*",
+               "*imperant*"),
+    
+    corrupt = c("*corromp*",
+                "*propagand*",
+                "*tronco*",
+                "*tracion*",
+                "*felon*",
+                "*vergüenz*",
+                "*escandal*",
+                "*verit*",
+                "*malhonnet*", 
+                "*mensong*",
+                "*falso*"),        
+    
+    integrit = c("*responsabilid*",
+                 "*autorizad*"),
+    
+    independ = c("*soberan*",
+                 "*neutral*"),
+    
+    dirdem = c("*democratic*",
+               "*referend*",
+               "*plebiscit*"),
+    
+    delib = c("*concens*",
+              "*compromis*"),
+    
+    represent = c("*representac*",
+                  "*parlement*",
+                  "*gobiern*"),
+    
+    antipop = c("*populist*",
+                "*demagog*")                                              
+    
+  ))        
+  
+  
+    
+    
+  pop_dict_ita <- dictionary(list(
+    
+    people = c("*popol*"),
+    
+    commun = c("*cittad*",
+               "*introduz*",
+               "*contribuent*",
+               "*comunit*"),
+    
+    elites = c("*elit*",
+               "*politic*",
+               "*establishment*",
+               "*partitocraz*",
+               "*regnant*",
+               "*imperant*"),
+    
+    corrupt = c("*corrot*",
+                "*propagand*",
+                "*ingann*",
+                "*tradi*",
+                "*fellon*",
+                "*vergogn*",
+                "*scandal*",
+                "*verita*",
+                "*disonest*",
+                "*bugi*",
+                "*fanfaluc*", 
+                "*fandon*"),        
+    
+    integrit = c("*responsabilit*",
+                 "*accreditat*"),
+    
+    independ = c("*sovran*",
+                 "*neutral*"),
+    
+    dirdem = c("*democratic*",
+               "*referend*",
+               "*plebiscit*",
+               "*popolar*"),
+    
+    delib = c("*concens*",
+              "*compromes*"),
+    
+    represent = c("*rappresentanz*",
+                  "*parlament*",
+                  "*govern*"),
+    
+    antipop = c("*populist*",
+                "*demagog*")                                              
+    
+  ))        
+  
+  
+  
