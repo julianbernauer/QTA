f43ed1982204f06e8c80f449becb73d6c2cd1abb
Julian Bernauer <julian.bernauer@googlemail.com>
2019-03-27 20:43:33 +0100

    Update birdscraping.r

--- covfefe/code/birdscraping.r
+++ covfefe/code/birdscraping.r
@@ -1,1 +1,25 @@
-## 
+## Some example code for getting Tweets with rtweet 
+
+library(rtweet)
+library(quanteda)
+
+# extracting the Tweets of three politicians 
+tmls_pops <- get_timelines(c("alice_weidel", "HCStracheFP", "NatalieRickli"), n = 1000)
+
+# Plot the frequency of tweets for each user over time
+tmls_pops %>%
+  dplyr::filter(created_at > "2018-09-01") %>%
+  dplyr::group_by(screen_name) %>%
+  ts_plot("days", trim = 1L) +
+  ggplot2::geom_point() +
+  ggplot2::theme_minimal() +
+  ggplot2::theme(
+    legend.title = ggplot2::element_blank(),
+    legend.position = "bottom",
+    plot.title = ggplot2::element_text(face = "bold")) +
+  ggplot2::labs(
+    x = NULL, y = NULL,
+    title = "Frequency of Twitter statuses posted by three populist politicians",
+    subtitle = "Twitter status (tweet) counts aggregated by day",
+    caption = "\nSource: Data collected from Twitter's REST API via rtweet"
+  )
